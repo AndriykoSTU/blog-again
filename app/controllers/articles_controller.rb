f8122ae5dc5bc7a97b1b9b3590fe90a43302b070
AndriykoSTU <balfagor@bigmir.net>
2014-10-17 00:25:02 +0300

    art

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+#coding:utf-8
+class ArticlesController < ApplicationController
+	include ArticlesHelper
+
+	def index
+		@articles = Article.all
+		
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Стаття '#{@article.title}' збережена!"
+
+		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+	@article = Article.find(params[:id])
+	@article.update(article_params)
+
+	flash.notice = "Стаття '#{@article.title}' оновлена!"
+
+	redirect_to articles_path(@article) end
+
+	def show
+		@article = Article.find(params[:id])
+
+	end
+	def destroy
+		@article = Article.destroy(params[:id])
+		flash.notice = "Статтю '#{@article.title}' видалено!"
+
+		redirect_to articles_path
+	end
+end
